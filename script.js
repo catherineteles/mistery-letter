ade7e7874da0cd89974728ca0df0ba0a66c632d4
Catherine Teles <cacateles12@gmail.com>
2022-01-01 16:15:07 -0300

    regex para espacos vazios

--- script.js
+++ script.js
@@ -7,21 +7,26 @@ let classeRotacao = ['rotateleft', 'rotateright'];
 let classeInclinacao = ['skewleft', 'skewright'];
 
 function createLetter(){
+    letter.innerHTML = '';
     let wordText = textoCarta.value;
     let wordList = wordText.split(' ');
-    if (wordText == ' ' || wordText == ''){
+    //https://stackoverflow.com/questions/10261986/how-to-detect-string-which-contains-only-spaces/50971250
+    if (wordText == '' || !wordText.replace(/\s/g, '').length){
         let alerta = document.createElement('span');
         alerta.innerText = 'Por favor, digite o conteúdo da carta.';
         letter.appendChild(alerta);
     }
-    for (let index = 0; index < wordList.length; index +=1){
-    let word = document.createElement('span');
-    word.innerText = wordList[index];
-    word.classList.add(classeEstilo[Math.floor(Math.random() * 4)]);
-    word.classList.add(classeTamanho[Math.floor(Math.random() * 4)]);
-    word.classList.add(classeRotacao[Math.floor(Math.random() * 3)]);
-    word.classList.add(classeInclinacao[Math.floor(Math.random() * 3)]);
-    letter.appendChild(word); 
+    else{
+        for (let index = 0; index < wordList.length; index +=1){
+        let word = document.createElement('span');
+        let estilo = classeEstilo[Math.floor(Math.random() * 4)];
+        let tamanho = classeTamanho[Math.floor(Math.random() * 4)];
+        let rotacao = classeRotacao[Math.floor(Math.random() * 3)];
+        let inclinacao = classeInclinacao[Math.floor(Math.random() * 3)];
+        word.innerText = wordList[index];
+        word.classList.add(estilo, tamanho, rotacao, inclinacao);
+        letter.appendChild(word); 
+        }
     }
 }
 
