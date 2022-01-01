c8bdfd60052f345ea8e88b3e6061e8625b59124d
Catherine Teles <cacateles12@gmail.com>
2022-01-01 15:48:35 -0300

    adição aleatória de classes

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let textoCarta = document.getElementById('carta-texto');
 const buttonCreate = document.getElementById('criar-carta');
 const letter = document.getElementById('carta-gerada');
+let classeEstilo = ['newspaper', 'magazine1', 'magazine2'];
+let classeTamanho = ['medium', 'big', 'reallybig'];
+let classeRotacao = ['rotateleft', 'rotateright'];
+let classeInclinacao = ['skewleft', 'skewright'];
 
 function createLetter(){
     let wordText = textoCarta.value;
@@ -13,6 +17,10 @@ function createLetter(){
     for (let index = 0; index < wordList.length; index +=1){
     let word = document.createElement('span');
     word.innerText = wordList[index];
+    word.classList.add(classeEstilo[Math.floor(Math.random() * 4)]);
+    word.classList.add(classeTamanho[Math.floor(Math.random() * 4)]);
+    word.classList.add(classeRotacao[Math.floor(Math.random() * 3)]);
+    word.classList.add(classeInclinacao[Math.floor(Math.random() * 3)]);
     letter.appendChild(word); 
     }
 }
